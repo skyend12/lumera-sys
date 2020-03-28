45302b6ae437533909092b4b763552dfcb163d9a
Cleonart <zredhard@gmail.com>
2020-03-28 15:47:06 +0800

    halaman tambah pengguna sudah fix

--- src/Layout/Sidebar.svelte
+++ src/Layout/Sidebar.svelte
@@ -117,10 +117,11 @@
       <hr class="mt-3 mb-3" />
       <!-- Sidebar Menu -->
       <nav class="mt-2">
+
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <li class="nav-item has-treeview menu-open">
+          <li class="has-treeview menu-open">
             <Link to = "dashboard">
-            <span class="nav-link active">
+            <span class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
--- src/Pages/TambahPengguna.svelte
+++ src/Pages/TambahPengguna.svelte
@@ -2,43 +2,47 @@
 	import { Router, Link, Route } from "svelte-routing";
 </script>
 
-<!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1></h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><Link to = "dashboard">Dashboard</Link></li>
-              <li class="breadcrumb-item"><Link to = "pengguna">Pengguna</Link></li>
-              <li class="breadcrumb-item active">Tambah Pengguna</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
+<style type="scss">
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <!-- left column -->
-          <div class="col-md-12">
-            <!-- general form elements -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title"><i class="fas fa-user-plus"></i>Tambah Pengguna</h3>
-              </div>
+</style>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="container">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1></h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><Link to = "dashboard">Dashboard</Link></li>
+            <li class="breadcrumb-item"><Link to = "pengguna">Pengguna</Link></li>
+            <li class="breadcrumb-item active">Tambah Pengguna</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-12">
+          <!-- general form elements -->
+          <div class="card card-primary">
+            <div class="card-header bg-white">
+              <h5 class="mt-1 mb-0"><i class="fas fa-user-plus mr-2"></i>Tambah Pengguna</h5>
+            </div>
               <!-- /.card-header -->
               <!-- form start -->
               <form role="form">
                 <div class="card-body">
                   <div class="form-group">
-                    <label for="name">Nama</label>
+                    <label for="name important-form">Nama</label>
                     <input type="text" class="form-control" id="name" placeholder="Masukkan Nama">
                   </div>
                   <div class="form-group">
@@ -50,23 +54,26 @@
                     <input type="text" class="form-control" id="password" placeholder="Masukkan Password">
                   </div>
                   <div class="form-group">
-                        <label>Hak Akses</label>
-                        <select class="form-control">
-                          <option>-PILIH-</option>
-                          <option>Administrator</option>
-                          <option>Kasir</option>
-                        </select>
-                      </div>
+                    <label>Hak Akses</label>
+                    <select class="form-control">
+                      <option>-PILIH-</option>
+                      <option>Administrator</option>
+                      <option>Kasir</option>
+                    </select>
+                  </div>
+                   <div class="form-group">
+                    <label for="password">Masukan Password Otentikasi Admin Anda</label>
+                    <input type="text" class="form-control" id="password" placeholder="Masukkan Password Otentikasi">
+                  </div>
                   
                 </div>
                 <!-- /.card-body -->
 
                 <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="submit" class="btn btn-primary">TAMBAHKAN PENGGUNA</button>
+                  <Link to="pengguna"><button type="submit" class="btn btn-danger">BATAL</button></Link>
                 </div>
               </form>
-  
-          
               
           </div>
           <!--/.col (right) -->
@@ -76,11 +83,3 @@
     </section>
     <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0
-    </div>
-    <strong>Copyright &copy; 2020 <a href="#">Lumera System</a>.</strong> All rights
-    reserved.
-  </footer>
